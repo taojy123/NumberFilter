1bfe436269d22cd44a65b985a109a90e8ea06446
Taojy <taojy0325@thundersoft.com>
2014-07-22 14:41:30 +0800

    finish2

--- Frame1.py
+++ Frame1.py
@@ -3,6 +3,8 @@
 
 import wx
 
+from numbers import Numbers
+
 def create(parent):
     return NumberFilter(parent)
 
@@ -15,29 +17,29 @@ class NumberFilter(wx.Frame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_NUMBERFILTER, name='NumberFilter',
-              parent=prnt, pos=wx.Point(398, 219), size=wx.Size(771, 358),
+              parent=prnt, pos=wx.Point(204, 184), size=wx.Size(826, 401),
               style=wx.DEFAULT_FRAME_STYLE, title='NumberFilter')
-        self.SetClientSize(wx.Size(755, 320))
+        self.SetClientSize(wx.Size(810, 363))
 
         self.panel1 = wx.Panel(id=wxID_NUMBERFILTERPANEL1, name='panel1',
-              parent=self, pos=wx.Point(0, 0), size=wx.Size(755, 320),
+              parent=self, pos=wx.Point(0, 0), size=wx.Size(810, 363),
               style=wx.TAB_TRAVERSAL)
         self.panel1.SetToolTipString('')
 
         self.textCtrl1 = wx.TextCtrl(id=wxID_NUMBERFILTERTEXTCTRL1,
-              name='textCtrl1', parent=self.panel1, pos=wx.Point(32, 64),
-              size=wx.Size(120, 176), style=wx.TE_MULTILINE,
-              value='12345\n56789\n48901\n74568\n21345\n51234\n94567\n25678\n14567\n34567')
+              name='textCtrl1', parent=self.panel1, pos=wx.Point(24, 64),
+              size=wx.Size(160, 216), style=wx.TE_MULTILINE,
+              value=u'04   06   10   11   05\n01   04   08   03   05\n03   11   01   08   10\n09   04   08   10   02\n01   04   11   08   05\n01   06   08   10   02\n03   05   10   07   09\n08   05   04   06   09\n08   11   09   02   07\n06   04   11   09   08')
         self.textCtrl1.SetToolTipString('')
 
         self.textCtrl2 = wx.TextCtrl(id=wxID_NUMBERFILTERTEXTCTRL2,
-              name='textCtrl2', parent=self.panel1, pos=wx.Point(168, 64),
-              size=wx.Size(552, 176), style=wx.TE_MULTILINE, value='')
+              name='textCtrl2', parent=self.panel1, pos=wx.Point(200, 64),
+              size=wx.Size(584, 216), style=wx.TE_MULTILINE, value='')
         self.textCtrl2.SetToolTipString('')
 
         self.button1 = wx.Button(id=wxID_NUMBERFILTERBUTTON1,
               label='\xc9\xb8\xd1\xa1\xbc\xc6\xcb\xe3', name='button1',
-              parent=self.panel1, pos=wx.Point(224, 264), size=wx.Size(248, 32),
+              parent=self.panel1, pos=wx.Point(240, 312), size=wx.Size(248, 32),
               style=0)
         self.button1.SetToolTipString('')
         self.button1.Bind(wx.EVT_BUTTON, self.OnButton1Button,
@@ -46,7 +48,7 @@ class NumberFilter(wx.Frame):
         self.staticText1 = wx.StaticText(id=wxID_NUMBERFILTERSTATICTEXT1,
               label='\xd6\xc7\xc4\xdc\xca\xfd\xd7\xd6\xc9\xb8\xd1\xa1\xd6\xfa\xca\xd6',
               name='staticText1', parent=self.panel1, pos=wx.Point(208, 16),
-              size=wx.Size(288, 35), style=0)
+              size=wx.Size(280, 35), style=0)
         self.staticText1.SetToolTipString('')
         self.staticText1.SetFont(wx.Font(26, wx.SWISS, wx.NORMAL, wx.NORMAL,
               False, '\xc1\xa5\xca\xe9'))
@@ -54,81 +56,12 @@ class NumberFilter(wx.Frame):
     def __init__(self, parent):
         self._init_ctrls(parent)
         
-    def is_in(self, num, nums):
-        if not num:
-            return False
-        for n in nums:
-            if not n:
-                continue
-            if num == n:
-                return True
-            if num == n[::-1]:
-                return True
-        return False
-    
-    def is_repeat(self, num, nums):
-        nums = ".".join(nums)
-        if num[0] in nums and num[1] in nums:
-            return True
-        return False
-    
-    def count_nums(self, nums):
-        return len(nums) - nums.count("")
-    
-    def remove_num(self, num, nums):
-        i = nums.index(num)
-        nums[i] = ""
-        if self.count_nums(nums) <= 4 and self.count_nums(nums) >= 2:
-            r = []
-            for n in nums:
-                if n:
-                    r.append(n)
-            self.textCtrl2.SetValue(self.textCtrl2.GetValue() + ".".join(r) + "    ")
-
-            
     def OnButton1Button(self, event):
         self.textCtrl2.SetValue("")
-        data = self.textCtrl1.GetValue()
-        data = data.strip()
-        lines = data.split("\n")
-        for i in range(len(lines[0]) - 1):
-            for j in range(i+1, len(lines[0])):
-                
-                self.textCtrl2.SetValue(self.textCtrl2.GetValue() + str(i+1) + str(j+1) + ":    ")
-                
-                nums = []
-                for n in lines:
-                    num = n[i] + n[j]
-                    nums.append(num)
-                
-                self.textCtrl2.SetValue(self.textCtrl2.GetValue() + ".".join(nums) + "    |    ")
-                    
-                for k in range(1, len(nums)):
-                    num = nums[k]
-                    if self.is_in(num, nums[:k]):
-                        self.remove_num(num, nums)
-                
-                                
-                while self.count_nums(nums) > 2:
-                                        
-                    flag = False
-                    
-                    for k in range(len(nums)):
-                        num = nums[k]
-                        if not num:
-                            continue
-                        if not self.is_repeat(num, nums[:k] + nums[k+1:]):
-                            self.remove_num(num, nums)
-                            flag = True
-                            
-                    if not flag:
-                        for num in nums:
-                            if num:
-                                self.remove_num(num, nums)
-                            
-                self.textCtrl2.SetValue(self.textCtrl2.GetValue() + "\n")
-                
-        self.textCtrl2.SetValue(self.textCtrl2.GetValue().strip()[:-1] + "*")
+        input_str = self.textCtrl1.GetValue().strip()
+        numbers = Numbers(input_str)
+        result_str = numbers.get_result()
+        self.textCtrl2.SetValue(result_str)
         event.Skip()
 
 
---
+++ numbers.py
@@ -0,0 +1,164 @@
+
+class Numbers(object):
+    """docstring for Numbers"""
+    def __init__(self, input_str):
+        input_str = input_str.strip()
+        lines = input_str.split("\n")
+        self.data = []
+        for line in lines:
+            temp_list = []
+            nums = line.split()
+            for num in nums:
+                num = num.strip()
+                temp_list.append(num)
+            self.data.append(temp_list)
+
+    @property
+    def lines(self):
+        lines = []
+        for i in range(len(self.data[0])-1):
+            for j in range(i+1, len(self.data[0])):
+                mark = "%d %d" % (i, j)
+                combs = []
+                for row in self.data:
+                    comb = Comb(row[i], row[j])
+                    combs.append(comb)
+                line = Line(combs, mark)
+                lines.append(line)
+        return lines
+
+    def get_result(self):
+        result_str = ""
+        for line in self.lines:
+            result4, result3, result2 = line.get_result()
+            result_str += "%s:    %s  |  %s    %s    %s\n" % (line.mark, str(line), result4, result3, result2)
+        result_str = result_str.strip()
+        return result_str[:-2] + "**" 
+
+
+
+class Comb(object):
+    """docstring for Comb"""
+    def __init__(self, num1, num2):
+        self.num1 = str(num1)
+        self.num2 = str(num2)
+
+    def __repr__(self):
+        return self.num1 + self.num2
+
+    def __eq__(self, other):
+        if isinstance(other, Comb):
+            return self.value == other.value
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__()
+
+    @property
+    def value(self):
+        return sorted([self.num1, self.num2])
+
+
+
+class Line(object):
+    """docstring for Line"""
+    def __init__(self, combs, mark=""):
+        self.combs = combs
+        self.original = combs[:]
+        self.mark = mark
+        self.result4 = []
+        self.result3 = []
+        self.result2 = []
+
+    def __repr__(self):
+        s = ""
+        for comb in self.original:
+            s += str(comb) + "."
+        return s[:-1]
+
+    @property
+    def line_singles(self):
+        singles = []
+        for comb in self.combs:
+            if not isinstance(comb, Comb):
+                continue
+            if comb.num1 not in singles:
+                singles.append(comb.num1)
+            if comb.num2 not in singles:
+                singles.append(comb.num2)
+        return sorted(singles)
+
+    @property
+    def count(self):
+        return len(self.line_singles)
+
+    def remove(self, comb):
+        self.combs.remove(comb)
+        if self.count == 4:
+            self.result4 = self.line_singles
+        elif self.count == 3:
+            self.result3 = self.line_singles
+        elif self.count == 2:
+            self.result2 = self.line_singles
+
+    def get_singles(self, temp_combs):
+        singles = []
+        for comb in temp_combs:
+            if comb.num1 not in singles:
+                singles.append(comb.num1)
+            if comb.num2 not in singles:
+                singles.append(comb.num2)
+        return singles
+
+    def is_repeat(self, comb):
+        temp_combs = self.combs[:]
+        temp_combs.remove(comb)
+        singles = self.get_singles(temp_combs)
+        if comb.num1 in singles and comb.num2 in singles:
+            return True
+        else:
+            return False
+
+    def filter1(self):
+        for i in range(len(self.combs)):
+            if self.combs[i] in self.combs[:i]:
+                self.combs[i] = "-"
+        while self.combs.count("-"):
+            self.remove("-")
+
+    def filter2(self):
+        for comb in self.combs:
+            if not self.is_repeat(comb):
+                self.remove(comb)
+                return
+        if self.combs:
+            self.remove(self.combs[0])
+
+    def get_result(self):
+        self.filter1()
+        while self.count >= 2:
+            self.filter2()
+        result4 = ".".join(self.result4)
+        result3 = ".".join(self.result3)
+        result2 = ".".join(self.result2)
+        return result4, result3, result2
+
+
+
+if __name__ == "__main__":
+
+    input_str = """04   06   10   11   05
+    01   04   08   03   05
+    03   11   01   08   10
+    09   04   08   10   02
+    01   04   11   08   05
+    01   06   08   10   02
+    03   05   10   07   09
+    08   05   04   06   09
+    08   11   09   02   07
+    06   04   11   09   08"""
+
+    n = Numbers(input_str)
+
+    print n.get_result()
+
